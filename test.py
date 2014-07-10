f8d5477e8f03fc2d97024119ae04e0e9d39eb07c
Will <wmparkin@gmail.com>
2014-07-10 13:31:51 -0400

    added comments

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import PySide
 print('Imported PySide version: ' + PySide.__version__)
+# PySide might not be linked to Shiboken correctly, this will check that it is.
 from PySide import QtGui
 print('Imported PySide.QtGui')
